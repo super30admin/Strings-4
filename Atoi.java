195f753f0e6b13e8cd1d08851aa1283d3e7993c6
gangakalburgi <56897430+gangakalburgi@users.noreply.github.com>
2021-04-12 17:44:21 -0700

    Create Atoi.java

---
+++ Atoi.java
@@ -0,0 +1,60 @@
+
+/*
+TC: O(n), n is length of string
+SC: O(1)
+
+*/
+class Solution {
+    public int myAtoi(String s) {
+        
+        
+        s = s.trim();
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        
+        int result = 0;
+        int limit = Integer.MAX_VALUE/10;
+        
+        char first = s.charAt(0);
+        if((first != '+' && first != '-') && !Character.isDigit(first)){
+            return 0;
+        }
+        
+        char sign = '+';
+        if(first == '-'){
+            sign = '-';
+        }
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(Character.isDigit(ch)){
+                if(sign == '+'){
+                    
+                    if(result > limit){
+                        return Integer.MAX_VALUE;
+                    }else if(result == limit && ch - '0' >= 7){
+                        return Integer.MAX_VALUE;
+                    }
+                    
+                }
+                else{
+                    if(result > limit){
+                        return Integer.MIN_VALUE;
+                    }else if(result == limit && ch - '0' >= 8){
+                        return Integer.MIN_VALUE;
+                    }
+                }
+                result = result * 10 + ch - '0';
+            }
+            else if(i != 0 ){
+                break;
+            }
+        }
+        
+        if(sign =='-'){
+            return -result;
+        }
+        return result;
+    }
+}
