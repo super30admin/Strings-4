1be767a28359f967f47fb2f0d47c0c80ba5e0ad0
gauravhjaisinghani <88104095+gauravhjaisinghani@users.noreply.github.com>
2022-02-07 02:49:43 -0500

    Strings 4 completed

---
+++ Solution2.java
@@ -0,0 +1,45 @@
+class Solution2 {
+    public int myAtoi(String s) {
+
+        //constant time and space
+        if(s == null) return 0;
+        s = s.trim();
+        if(s.length() == 0) return 0;
+                char first = s.charAt(0);
+
+        char sign = '+';
+        if(first == '-'){
+            sign = '-';
+        }
+        if((first != '+'  && first != '-') && !Character.isDigit(first)) return 0;
+        int result =0; int limit = Integer.MAX_VALUE / 10;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+             if(Character.isDigit(c)){
+                 //character is digit
+                 if(sign == '+'){
+                     if(result > limit){
+                         return Integer.MAX_VALUE;
+                     } else if(result == limit){
+                         if(c - '0' > 7){
+                             return Integer.MAX_VALUE;
+                         }
+                     } 
+                 } else {
+                     if(result > limit){
+                         return Integer.MIN_VALUE;
+                     } else if(result == limit){
+                         if(c - '0' > 8){
+                             return Integer.MIN_VALUE;
+                         }
+                     }
+                 }
+                 result = result * 10 + (c - '0');
+            } else if(i != 0){
+                 break;
+             }
+        }
+        if(sign == '-') return -result;
+        return result;
+    }
+}
