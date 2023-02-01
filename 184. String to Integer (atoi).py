abc5fae8dd826745c38b1bc94f1cbdaacb25d276
Sanket Bagul <sbagul077@gmail.com>
2023-02-01 15:01:27 -0500

    Update Time Complexity
    Made correction to Time Complexity from O(1) to O(n).

--- 184. String to Integer (atoi).py
+++ 184. String to Integer (atoi).py
@@ -42,7 +42,7 @@ class Solution:
 
         return result
 
-# Time Complexity: O(1)
+# Time Complexity: O(n)
 # Space Complexity: O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
